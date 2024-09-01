1b443135256cacaabb10534513d38dbb913617b7
Sameeksh Thakur <SameekshThakur@gmail.com>
2024-09-01 15:17:33 +0530

    files udated

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL=import.meta.env.VITE_URL || "https://blogify-unnm.onrender.com";
-export const IF=import.meta.env.VITE_IF || "https://blogify-unnm.onrender.com/images/";
+export const URL="https://blogify-unnm.onrender.com";
+export const IF="https://blogify-unnm.onrender.com/images/";
