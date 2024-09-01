bc2efcf7243cccffdc16b22aff0795cb01d74f9b
Sameeksh Thakur <SameekshThakur@gmail.com>
2024-09-01 14:52:07 +0530

    url changed

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,2 @@
-export const URL=import.meta.env.VITE_URL
-export const IF=import.meta.env.VITE_IF
+export const URL=import.meta.env.VITE_URL || "https://blogify-unnm.onrender.com";
+export const IF=import.meta.env.VITE_IF || "https://blogify-unnm.onrender.com/images/";
